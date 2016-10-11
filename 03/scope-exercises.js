bc0b6c62ed33113013833e190a19defe663607ed
Marcell Endrey <marcell.endrey@prezi.com>
2016-10-11 22:05:59 +0200

    [03] variable scope exercises

---
+++ 03/scope-exercises.js
@@ -0,0 +1,63 @@
+// 0. --------------------------------------------
+function sing() {
+    alert('Do you want to build a snowman?');
+}
+sing();
+
+// 1. --------------------------------------------
+var what = "snowman";
+function sing() {
+    alert('Do you want to build a ' + what + '?');
+}
+sing();
+
+// 2. --------------------------------------------
+var what = "fire";
+function sing(what) {
+    alert('Do you want to build a ' + what + '?');
+}
+sing(what);
+
+// 3. --------------------------------------------
+var invention = "spaceship";
+function sing(what) {
+    alert('Do you want to build a ' + what + '?');
+}
+sing(invention);
+
+// 4. --------------------------------------------
+var idea = "house";
+function sing(idea) {
+    alert('Do you want to build a ' + what + '?');
+}
+sing(idea);
+
+// 5. --------------------------------------------
+var what = "castle";
+function sing(what) {
+    alert('Do you want to build a ' + what + '?');
+}
+sing();
+
+// 6. --------------------------------------------
+var what = "fire";
+function sing(what) {
+    alert('Do you want to build a ' + what + '?');
+}
+sing('what');
+
+// 7. --------------------------------------------
+var what = "LEGO airplane";
+function sing() {
+    var what = "kennel for a pitbull"
+    alert('Do you want to build a ' + what + '?');
+}
+sing();
+
+// 8. --------------------------------------------
+var what = "drone";
+function sing(what) {
+    var what = "better future for our children"
+    alert('Do you want to build a ' + what + '?');
+}
+sing(what);
