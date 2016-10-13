8b8b2017e1b5cfc986a272a5da6cdab7f21fd7bf
Marcell Endrey <marcell.endrey@prezi.com>
2016-10-13 14:52:54 +0200

    [03] make exercises easier to read, hopefully

--- 03/scope-exercises.js
+++ 03/scope-exercises.js
@@ -5,59 +5,59 @@ function sing() {
 sing();
 
 // 1. --------------------------------------------
-var what = "snowman";
 function sing() {
     alert('Do you want to build a ' + what + '?');
 }
+var what = "snowman";
 sing();
 
 // 2. --------------------------------------------
-var what = "fire";
 function sing(what) {
     alert('Do you want to build a ' + what + '?');
 }
+var what = "fire";
 sing(what);
 
 // 3. --------------------------------------------
-var invention = "spaceship";
 function sing(what) {
     alert('Do you want to build a ' + what + '?');
 }
+var invention = "spaceship";
 sing(invention);
 
 // 4. --------------------------------------------
-var idea = "house";
 function sing(idea) {
     alert('Do you want to build a ' + what + '?');
 }
+var idea = "house";
 sing(idea);
 
 // 5. --------------------------------------------
-var what = "castle";
 function sing(what) {
     alert('Do you want to build a ' + what + '?');
 }
+var what = "castle";
 sing();
 
 // 6. --------------------------------------------
-var what = "fire";
 function sing(what) {
     alert('Do you want to build a ' + what + '?');
 }
+var what = "fire";
 sing('what');
 
 // 7. --------------------------------------------
-var what = "LEGO airplane";
 function sing() {
     var what = "kennel for a pitbull"
     alert('Do you want to build a ' + what + '?');
 }
+var what = "LEGO airplane";
 sing();
 
 // 8. --------------------------------------------
-var what = "drone";
 function sing(what) {
     var what = "better future for our children"
     alert('Do you want to build a ' + what + '?');
 }
+var what = "drone";
 sing(what);
