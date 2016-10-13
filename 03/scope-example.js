c08ed08530fb953371c42e96859b22f0869c0e24
Marcell Endrey <marcell.endrey@prezi.com>
2016-10-13 14:50:19 +0200

    [03] update examples to make them easier to read

--- 03/scope-example.js
+++ 03/scope-example.js
@@ -1,33 +1,36 @@
 // 1. Külső Sanyi
 // A függvényen belüli 'size' a külső változóra vonatkozik
 
-function centeredCircle() {
-    ellipse(windowWidth / 2, windowHeight / 2, size, size);
+function redCircle() {
+    fill('red');
+    centeredCircle(size);
 }
 
 var size = 100;
 
-centeredCircle();
+redCircle();
 
 //----------------------------------------------------------
 // 2. Kölcsönsanyi
 // A bemeneti adat (argumentum) a függvényen belül a 'size' nevet kapja
 
-function centeredCircle(size) {
-    ellipse(windowWidth / 2, windowHeight / 2, size, size);
+function redCircle(size) {
+    fill('red');
+    centeredCircle(size);
 }
 
 var pizza = 100;
 
-centeredCircle(pizza);
+redCircle(pizza);
 
 //----------------------------------------------------------
 // 3. Saját Sanyi
 // A függvénynek van egy saját 'size' nevű változója
 
-function centeredCircle() {
+function redCircle() {
+    fill('red');
     var size = 100;
-    ellipse(windowWidth / 2, windowHeight / 2, size, size);
+    centeredCircle(size);
 }
 
-centeredCircle();
+redCircle();
--- 03/scope-precedence.js
+++ 03/scope-precedence.js
@@ -1,37 +1,40 @@
 // 1. Külső Sanyi
 // A függvényen belüli 'size' a külső változóra vonatkozik
 
-function centeredCircle() {
-    ellipse(windowWidth / 2, windowHeight / 2, size, size);
+function redCircle() {
+    fill('red');
+    centeredCircle(size);
 }
 
 var size = 100;
 
-centeredCircle();
+redCircle();
 
 //----------------------------------------------------------
 // 2. Kölcsönsanyi
 // Az argumentum felülírja a külső változót
 
-function centeredCircle(size) {
-    ellipse(windowWidth / 2, windowHeight / 2, size, size);
+function redCircle(size) {
+    fill('red');
+    centeredCircle(size);
 }
 
 var size = 1000;
 var pizza = 100;
 
-centeredCircle(pizza);
+redCircle(pizza);
 
 //----------------------------------------------------------
 // 3. Saját Sanyi
 // A saját változó felülírja az argumentumot és a külső változót
 
-function centeredCircle(size) {
+function redCircle(size) {
     var size = 100;
-    ellipse(windowWidth / 2, windowHeight / 2, size, size);
+    fill('red');
+    centeredCircle(size);
 }
 
 var size = 1000;
 var pizza = 100;
 
-centeredCircle();
+redCircle();
