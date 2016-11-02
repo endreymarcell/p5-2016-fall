3702f23e684107b14231b344d5d63d5659e2b7a5
Marcell Endrey <marcell.endrey@prezi.com>
2016-11-02 20:11:53 +0100

    [06] code formatting examples

---
+++ 06/formatting-examples/03-indented-but-dense.js
@@ -0,0 +1,17 @@
+var animal;
+var x=0;
+
+function setup(){
+    createCanvas(windowWidth,windowHeight);
+    animal=loadImage('guinea-pig.png');
+}
+
+function draw() {
+    background('white');
+    if(mouseX<x){
+        x=x-10;
+    }else{
+        x=x+10;
+    }
+    image(animal,x,300);
+}
